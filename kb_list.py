a50c994e3c0f556d0de9741f57a77d7be85fd07c
Arthur Demarchi <arthurdemarchi@outlook.com>
2021-01-29 21:58:46 -0300

    finalizing code

--- kb_list.py
+++ kb_list.py
@@ -11,96 +11,124 @@ RESET_READING_TIMEOUT = 1
 NEXT_CARD_KEY = 13
 CARD_DATA_LENGTH = 16
 CARD_LENGTH = 8
-STOP_KEY = 32
+GET_URL = "http://localhost:5000/api/v1/resources/search?badge="
+POST_URL = "https://webhook.site/374dcafe-4c83-47db-8a3e-a6d81b8f0125"
 
 # global Vars
 timer = datetime.now()
-last_card = ''
-card_id = ''
+card_data = ''
+keyboard_stream = ''
 running = True
 end_reading = False
 
 # callback function when key is stroked
 def kbevent(event):
-    #inserting global variables in this scope
-    global NEXT_CARD_KEY
-    global CARD_DATA_LENGTH
-    global STOP_KEY
-    global running
-    global end_reading
-    global timer
-    global last_card
-    global card_id
+    """function that concatanates last stroked
+        key into global variable keyboard_stream.
+        
+        Checks if keyboard_stream already has enougth
+        length to be considered a card_data, in wich case
+        it replaces the global variable card_data and 
+        raises the end_reading flag.
+    """
 
-    #insert key into card id
-    card_id = card_id + chr(event.Ascii)
-
-    #if stop key is pressed stop process
-    if event.Ascii == STOP_KEY:
-        running = False
-
-    #if end of reading key is stroked terminates current reading
-    if event.Ascii == NEXT_CARD_KEY:
-        end_reading = True
-        last_card = card_id
-        card_id = ''
-
-    #if card maximum length s reached terminates current reading
-    if len(card_id) >= CARD_DATA_LENGTH:
-        end_reading = True
-        last_card = card_id
-        card_id = ''
-
-    #resets timer
-    timer = datetime.now()
-
-#    except Exception as e:
-#        print('INFO: Could not interpret last key ASCII code')
-#        print('EXCEPTION: ', e)
-#        card_id = ''
-#        end_reading = True
-#        timer = datetime.now()
-#        return
-
-
-def allow_entrance(card):
     try:
-        r = requests.get('http://localhost:5000/api/v1/resources/search?badge=' + str(card))
+        #inserting global variables in this scope
+        global CARD_DATA_LENGTH
+        global running
+        global end_reading
+        global timer
+        global card_data
+        global keyboard_stream
+
+        #insert key into card id
+        keyboard_stream = keyboard_stream + chr(event.Ascii)
+
+        #if card maximum length s reached terminates current reading
+        if len(keyboard_stream) >= CARD_DATA_LENGTH:
+            end_reading = True
+            card_data = keyboard_stream
+            keyboard_stream = ''
+
+        #resets timer
+        timer = datetime.now()
+
+    #in case of exception do nothing  
+    except Exception as e:
+        print('INFO: Keystroke Event Failed')
+        print('EXCEPTION: ', traceback.print_exc(type(e), e, e._traceback_))
+
+# network access function
+def allow_entrance(card):
+    """ function that gets the employee data from 
+        the url of the api, using a get, discriminate
+        pid data, and post pid data to another api
+        that allows the employee to enter the facilities
+    """
+    try:
+        #get global settings
+        global GET_URL
+        global POST_URL
+
+        #network communication
+        r = requests.get(GET_URL + str(card))
         r = r.json()
-        r = requests.post("https://webhook.site/374dcafe-4c83-47db-8a3e-a6d81b8f0125", json={'pid' : r['pid']})
+        r = requests.post(POST_URL, json={'pid' : r['pid']})
+
+    #in case of exception do nothing
     except Exception as e:
         print('INFO: Request Failed')
         print('EXCEPTION: ', traceback.print_exc())
-        return
 
-    return
-
+#filters card_id from card_data
 def filter(card_data):
+    """ checks if card_data has right length
+    takes the n last numbers from card_data,
+    checks if all digits are numbers,
+    both cases check cases return negative numbers,
+    this is importarnt because all valid card_id
+    are positive numbers, and returned as int.
+    use this inormation to check if card was valid
+    """
     try:
+        #check for length
         if len(card_data) != CARD_DATA_LENGTH:
             print("Dumping Reading: Read Data has Wrong Length")
             return -1
-        
-        card = card_data[CARD_DATA_LENGTH-CARD_LENGTH: CARD_DATA_LENGTH: 1]
 
-        if not str.isdecimal(card):
+        #takes last n digits
+        card_id = card_data[CARD_DATA_LENGTH-CARD_LENGTH: CARD_DATA_LENGTH: 1]
+
+        #check for nan characters
+        if not str.isdecimal(card_id):
             print("Dumping Reading: Read Data has Letters or Special Characters")
             return -2
+        
+        #return card_id
+        return int(card_id)
 
-        return int(card)
 
+    #in case of exception do nothing
     except Exception as e:
         print('INFO: Could not Filter card from card_data')
-        print('EXCEPTION: ', e)
+        print('EXCEPTION: ', traceback.print_exc())
+        return -3
 
+#main function
 def main():
-    #inserting global 
-    #' RESET_READING_TIMEOUT
+    """ This api keeps listening to key strokes
+    and tries to identfie card_id comming from them
+    if a card id is identified, it posts the empolyee
+    pid somerwhere else
+    """
+
+    ### INIT
+    #inserting global variabes
     global running
     global end_reading
     global timer
-    global last_card
-    global card_id
+    global card_data
+    global keyboard_stream
 
     # Create hookmanager
     hookman = pyxhook.HookManager()
@@ -114,33 +142,43 @@ def main():
     # Start our listener
     hookman.start()
 
+    ### BODY
     # Create a loop to keep the application running
     while running:
         try:
-            #sleeps
+            #sleeps to wait for new keystroke events
             time.sleep(0.1)
+
             #if timer reaches timeout terminates reading
+            #rhis is used as a filter for human typing
             if (datetime.now()- timer).seconds > RESET_READING_TIMEOUT:
                 #if timeout is reached during a read, saves read data
-                if card_id != '':
-                    last_card = card_id
+                if keyboard_stream != '':
+                    card_data = keyboard_stream
                     end_reading = True
-                    card_id = ''
+                    keyboard_stream = ''
 
-                #resets timer
+                #always resets timer
                 timer = datetime.now()
-            #if terminates reading flag is up, do what its suppose to do
+
+            #if the end_reading flag was raised, call filter and allow entrance
             if end_reading:
+                #changes flag status
                 end_reading = False
-                card = filter(last_card)
-                if card >= 0:
-                    allow_entrance(card)
-        
+                #filter card_data
+                card_id = filter(card_data)
+                #if filter returned a valid id, calls allow_entrance
+                if card_id >= 0:
+                    allow_entrance(card_id)
+    
+        #in case of exception do nothing
         except Exception as e:
             print('INFO: API Iteration lost')
-            print('EXCPETION: ', e)
+            print('EXCEPTION: ', traceback.print_exc())
 
-    # Close the listener when we are done
+    ### CLOSING
+    # Close the listener when loop is donne
     hookman.cancel()
 
+#call main
 main()
